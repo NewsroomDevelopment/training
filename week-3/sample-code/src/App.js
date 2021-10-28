f1ce28b256213795c427b62e0d950cc1c85e0756
Erin <erin2722@gmail.com>
2021-10-27 21:23:59 -0400

    :shipit: Changed during lecture

--- week-3/sample-code/src/App.js
+++ week-3/sample-code/src/App.js
@@ -5,60 +5,72 @@ import Input from './components/input';
 function App() {
   const [todos, setToDos] = useState([]);
 
-  useEffect(() => fetchData());
+  useEffect(() => fetchData(), []);
 
   const fetchData = async () => {
-    fetch(`/api/todos`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }})
-        .then(response => {
-            if (response.status >= 200 && response.status < 300) {
-                return response;
-              }
-              const error = new Error(`HTTP Error ${response.statusText}`);
-              error.status = response.statusText;
-              error.response = response;
-              console.log(error);
-              throw error;
-        })
-        .then(response => response.json())
-        .then(json => {
-            console.log(json);
-            setToDos(json.data);
-        })
-        .catch(error => console.log(error));    
+    // fetch(`/api/todos`, {
+    //     method: 'GET',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }})
+    //     .then(response => {
+    //         if (response.status >= 200 && response.status < 300) {
+    //             return response;
+    //           }
+    //           const error = new Error(`HTTP Error ${response.statusText}`);
+    //           error.status = response.statusText;
+    //           error.response = response;
+    //           console.log(error);
+    //           throw error;
+    //     })
+    //     .then(response => response.json())
+    //     .then(json => {
+    //         console.log(json);
+    //         setToDos(json.data);
+    //     })
+    //     .catch(error => console.log(error));    
+
+    // From 10/27 lecture
+    setToDos([{id: 1, title: "todo"}]);
   };
 
   let current_id = todos.length + 1;
   
 
   const deleteToDo = (id) => {
-    fetch(`/api/remove_todo/${id}`, {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      }})
-      .then(response => {
-          if (response.status >= 200 && response.status < 300) {
-              return response;
-            }
-            const error = new Error(`HTTP Error ${response.statusText}`);
-            error.status = response.statusText;
-            error.response = response;
-            console.log(error);
-            throw error;
-      })
-      .then(response => response.json())
-      .then(json => {
-          console.log(json);
-          setToDos(json.data);
-      })
-      .catch(error => console.log(error));
+    // From 10/27 lecture
+    const newTodoList = todos.filter(todo => todo.id !== id);
+
+    setToDos(newTodoList);
+
+
+
+
+    // fetch(`/api/remove_todo/${id}`, {
+    //   method: 'POST',
+    //   headers: {
+    //       'Content-Type': 'application/json'
+    //   }})
+    //   .then(response => {
+    //       if (response.status >= 200 && response.status < 300) {
+    //           return response;
+    //         }
+    //         const error = new Error(`HTTP Error ${response.statusText}`);
+    //         error.status = response.statusText;
+    //         error.response = response;
+    //         console.log(error);
+    //         throw error;
+    //   })
+    //   .then(response => response.json())
+    //   .then(json => {
+    //       console.log(json);
+    //       setToDos(json.data);
+    //   })
+    //   .catch(error => console.log(error));
   };
 
   const submitTodo = (todo) => {
+    // From 10/27 lecture
     const newTodo = {
       "userId": 1,
       "id": current_id,
@@ -67,30 +79,31 @@ function App() {
     };
 
     current_id += 1;
-    // add a link to explain the spread operator
 
-    fetch(`/api/add_todo`, {
-      method: 'POST',
-      body: JSON.stringify({todo: newTodo}),
-      headers: {
-          'Content-Type': 'application/json'
-      }})
-      .then(response => {
-          if (response.status >= 200 && response.status < 300) {
-              return response;
-            }
-            const error = new Error(`HTTP Error ${response.statusText}`);
-            error.status = response.statusText;
-            error.response = response;
-            console.log(error);
-            throw error;
-      })
-      .then(response => response.json())
-      .then(json => {
-          console.log(json);
-          setToDos(json.data);
-      })
-      .catch(error => console.log(error));
+    setToDos([...todos, newTodo]);
+
+    // fetch(`/api/add_todo`, {
+    //   method: 'POST',
+    //   body: JSON.stringify({todo: newTodo}),
+    //   headers: {
+    //       'Content-Type': 'application/json'
+    //   }})
+    //   .then(response => {
+    //       if (response.status >= 200 && response.status < 300) {
+    //           return response;
+    //         }
+    //         const error = new Error(`HTTP Error ${response.statusText}`);
+    //         error.status = response.statusText;
+    //         error.response = response;
+    //         console.log(error);
+    //         throw error;
+    //   })
+    //   .then(response => response.json())
+    //   .then(json => {
+    //       console.log(json);
+    //       setToDos(json.data);
+    //   })
+    //   .catch(error => console.log(error));
   };
 
   return (
